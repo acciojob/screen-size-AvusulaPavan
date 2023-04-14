3af8a10fc5c417d2b12978d612642214c55a931e
AvusulaPavanChary <120267773+AvusulaPavan@users.noreply.github.com>
2023-04-14 13:41:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const width = window.innerWidth;
+      const height = window.innerHeight;
+
+      // set the values in the corresponding span elements
+      document.getElementById("width").textContent = width;
+      document.getElementById("height").textContent = height;
